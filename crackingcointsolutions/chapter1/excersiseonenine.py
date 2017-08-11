04fee5ebe7e6db5f3bdc118a9f136c3739f7f694
Igor Oya <igor.oya.vallejo@desy.de>
2017-08-11 18:55:08 +0200

    add new items

---
+++ crackingcointsolutions/chapter1/excersiseonenine.py
@@ -0,0 +1,11 @@
+'''
+Created on 11 Aug 2017
+String rotation: Assume you have a method isSubstring which checks if one word is a substring
+of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one
+call to isSubstring (e.g. "waterbottle" is rotation of "erbottlewat)
+
+@author: igoroya
+'''
+
+if __name__ == '__main__':
+    pass
